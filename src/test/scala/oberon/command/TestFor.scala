5aa9b98eead734cde82af6238dd2b259f264f5a1
Zigfreud <thalesmenezes13@gmail.com>
2018-05-16 17:51:11 -0300

    Testes para For

--- src/test/scala/oberon/command/TestFor.scala
+++ src/test/scala/oberon/command/TestFor.scala
@@ -20,10 +20,13 @@ class TestFor extends FlatSpec with Matchers with GivenWhenThen with BeforeAndAf
     clear()
   }
   // soma;
-  // for(soma := 0, x := 1; x <= 10; x++) begin
+  // for(soma := 0, x := 1; x < 11; x++) begin
   //   soma := soma + x;
   // end
   // print(soma);
+  //
+  // or ...
+  // for(soma := 0, x := 1; x < 11; {x++; soma := soma + x;})
   it should "lookup(soma) must be equal to 55 after a loop summing up 1 to 10" in {
     val d1 = new Declaration("soma")                 // soma;
     val a1 = new Assignment("soma", IntValue(0))     // soma := 0;
@@ -47,6 +50,39 @@ class TestFor extends FlatSpec with Matchers with GivenWhenThen with BeforeAndAf
       case None => 40 should be (40)
       case _ => 0 should be (1)
     }
+  }
 
+  it should "show (x == 10) after for(x := 0; x < 10; x++)" in{
+    val d1 = new Declaration("x")                                                   //x;
+    val a1 = new Assignment("x",IntValue(0))                                        //x := 0;
+    val a2 = new Assignment("x",new AddExpression(new VarRef("x"),IntValue(1)))     //x++;
+    val cond = new SlExpression(new VarRef("x"),IntValue(10))                       //x < 10
+    val f1 = new For(new BlockCommand(List(a1)), cond, new BlockCommand(List(a2)))
+    
+    d1.run()
+    f1.run()
+    
+    val result = lookup("x")
+    result match {
+        case Some(v) => v.eval() should be (IntValue(10))
+        case _ => -1 should be (0)
+    }
+  }
+  
+  it should "show (x == 1) after for(x := 10; x > 1; x--)" in{
+    val d1 = new Declaration("x")                                                     //x;
+    val a1 = new Assignment ("x",IntValue(10))                                        //x := 10;
+    val a2 = new Assignment ("x", new SubExpression(new VarRef("x"),IntValue(1)))     //x--;
+    val cond = new SgExpression(new VarRef("x"),IntValue(1))                          //x > 1
+    val f1 = new For(new BlockCommand(List(a1)), cond, new BlockCommand(List(a2)))
+    
+    d1.run()
+    f1.run()
+    
+    val result = lookup("x")
+    result match {
+        case Some(v) => v.eval() should be (IntValue(1))
+        case _ => -1 should be (0)
+    }
   }
 }
